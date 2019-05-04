9b57f10784db8f48f05568eb38eb040dbfb451cc
Bamdad Sabbagh <dev@bamdadsabbagh.com>
2019-05-04 19:37:05 +0200

    example

--- liveView/js/index.js
+++ liveView/js/index.js
@@ -1,78 +1,89 @@
-let camera
-let scene
-let renderer
-let geometry
-let material
-let mesh
-let container
-
-function init () {
-
-  let light
-
-  container = document.createElement( 'div' );
-  document.body.appendChild( container );
-
-  //camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10)
-  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
-  camera.position.z = 1
-
-  scene = new THREE.Scene()
-  scene.background = new THREE.Color( 0xf0f0f0 )
-
-  light = new THREE.DirectionalLight( 0xffffff, 1 )
-  light.position.set( 1, 1, 1 ).normalize()
-  scene.add( light )
-
-
-  geometry = new THREE.BoxBufferGeometry(0.2, 0.2, 0.2)
-
-  for ( var i = 0; i < 100; i ++ ) {
-    var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
-    //var object = new THREE.Mesh( geometry, new THREE.MeshNormalMaterial();
-    object.position.x = Math.random() * 800 - 400;
-    object.position.y = Math.random() * 800 - 400;
-    object.position.z = Math.random() * 800 - 400;
-    object.rotation.x = Math.random() * 2 * Math.PI;
-    object.rotation.y = Math.random() * 2 * Math.PI;
-    object.rotation.z = Math.random() * 2 * Math.PI;
-    object.scale.x = Math.random() + 0.5;
-    object.scale.y = Math.random() + 0.5;
-    object.scale.z = Math.random() + 0.5;
-    scene.add( object );
-  }
-
-
-  //material = new THREE.MeshNormalMaterial()
-
-  //mesh = new THREE.Mesh(geometry, material)
-  //scene.add(mesh)
-
-
-  raycaster = new THREE.Raycaster();
-
-
-  renderer = new THREE.WebGLRenderer();
-  //renderer = new THREE.WebGLRenderer({ antialias: true })
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  //document.body.appendChild(renderer.domElement)
-
-  container.appendChild( renderer.domElement );
-
-
-}
-
-function animate () {
-  requestAnimationFrame(animate)
-
-  mesh.rotation.x += 0.01
-  mesh.rotation.y += 0.02
-  //mesh.translation.x += 0.01
-
-  renderer.render(scene, camera)
-}
+var container, stats
+var camera, scene, raycaster, renderer
+var mouse = new THREE.Vector2(), INTERSECTED
+var radius = 100, theta = 0
 
 init()
-//animate()
+animate()
+
+function init () {
+  container = document.createElement('div')
+  document.body.appendChild(container)
+  var info = document.createElement('div')
+  info.style.position = 'absolute'
+  info.style.top = '10px'
+  info.style.width = '100%'
+  info.style.textAlign = 'center'
+  info.innerHTML = '<a href="http://threejs.org" target="_blank" rel="noopener">three.js</a> webgl - interactive cubes'
+  container.appendChild(info)
+  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 10000)
+  scene = new THREE.Scene()
+  scene.background = new THREE.Color(0xf0f0f0)
+  var light = new THREE.DirectionalLight(0xffffff, 1)
+  light.position.set(1, 1, 1).normalize()
+  scene.add(light)
+  var geometry = new THREE.BoxBufferGeometry(20, 20, 20)
+  for (var i = 0; i < 2000; i++) {
+    var object = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff }))
+    object.position.x = Math.random() * 800 - 400
+    object.position.y = Math.random() * 800 - 400
+    object.position.z = Math.random() * 800 - 400
+    object.rotation.x = Math.random() * 2 * Math.PI
+    object.rotation.y = Math.random() * 2 * Math.PI
+    object.rotation.z = Math.random() * 2 * Math.PI
+    object.scale.x = Math.random() + 0.5
+    object.scale.y = Math.random() + 0.5
+    object.scale.z = Math.random() + 0.5
+    scene.add(object)
+  }
+  raycaster = new THREE.Raycaster()
+  renderer = new THREE.WebGLRenderer()
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  container.appendChild(renderer.domElement)
+  document.addEventListener('mousemove', onDocumentMouseMove, false)
+  //
+  window.addEventListener('resize', onWindowResize, false)
+}
+
+function onWindowResize () {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+}
+
+function onDocumentMouseMove (event) {
+  event.preventDefault()
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
+}
+
+//
+function animate () {
+  requestAnimationFrame(animate)
+  render()
+}
+
+function render () {
+  theta += 0.1
+  camera.position.x = radius * Math.sin(THREE.Math.degToRad(theta))
+  camera.position.y = radius * Math.sin(THREE.Math.degToRad(theta))
+  camera.position.z = radius * Math.cos(THREE.Math.degToRad(theta))
+  camera.lookAt(scene.position)
+  camera.updateMatrixWorld()
+  // find intersections
+  raycaster.setFromCamera(mouse, camera)
+  var intersects = raycaster.intersectObjects(scene.children)
+  if (intersects.length > 0) {
+    if (INTERSECTED != intersects[0].object) {
+      if (INTERSECTED) INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex)
+      INTERSECTED = intersects[0].object
+      INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex()
+      INTERSECTED.material.emissive.setHex(0xff0000)
+    }
+  } else {
+    if (INTERSECTED) INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex)
+    INTERSECTED = null
+  }
+  renderer.render(scene, camera)
+}
