120417ffe97541b4aaa5a196e8c658527a3c3766
Bamdad Sabbagh <dev@bamdadsabbagh.com>
2019-05-07 12:52:50 +0200

    testing new git/hooks

--- hooks/git.sh
+++ hooks/git.sh
@@ -6,6 +6,6 @@
 echo "add, commit & push - Please input commit message then press [ENTER]"
 read commit
 
-query="git add . && git commit -m $commit && git push"
+query="yarn fix && git add . && git commit -m $commit && git push"
 
 eval "$query"
