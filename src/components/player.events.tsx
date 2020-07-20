249701baa8c83c89256347c89f8460c38fc21fa3
Bamdad Sabbagh <git@bamdadsabbagh.com>
2020-07-20 14:08:33 +0200

    fix: refactored player

--- src/components/player.events.tsx
+++ src/components/player.events.tsx
@@ -1,9 +1,22 @@
-import React, { ReactElement, useCallback } from 'react'
+import React, { ReactElement, useCallback, useEffect } from 'react'
 import KeyboardEventHandler from 'react-keyboard-event-handler'
-import { useRecoilValue } from 'recoil'
-import { playerStateIsPlaying, playerStateRef } from './player.state'
+import { useRecoilValue, useRecoilState } from 'recoil'
+import { createBrowserHistory } from 'history'
+import { playerStateIsPlaying, playerStateRef, playerStateSpeed, playerStateSource } from './player.state'
 
 export const PlayerEvents = (): ReactElement => {
+
+    return (
+        <>
+            <PlayerEventsOnKeyboard />
+            <PlayerEventsOnSpeedChange />
+            <PlayerEventsOnMount />
+        </>
+    )
+
+}
+
+const PlayerEventsOnKeyboard = (): ReactElement => {
 
     const isPlaying = useRecoilValue (playerStateIsPlaying)
     const ref = useRecoilValue (playerStateRef)
@@ -26,3 +39,53 @@ export const PlayerEvents = (): ReactElement => {
 
 }
 
+const PlayerEventsOnSpeedChange = (): ReactElement => {
+
+    const audio = useRecoilValue (playerStateRef)
+    const speed = useRecoilValue (playerStateSpeed)
+
+    useEffect (() => {
+
+        audio.playbackRate = speed
+        
+        const history = createBrowserHistory ()
+            
+        history.push (`${window.location.pathname}?speed=${speed}`)
+
+    }, [speed, audio, audio.src])
+
+    return <></>
+
+}
+
+const PlayerEventsOnMount = (): ReactElement => {
+
+    const [, setIsPlaying] = useRecoilState (playerStateIsPlaying)
+    const playerRef = useRecoilValue (playerStateRef)
+    const source = useRecoilValue (playerStateSource)
+
+    useEffect (() => {
+
+        const audio: any = playerRef
+
+        audio.mozPreservesPitch = false
+        
+        audio.src = source
+        
+        audio.load ()
+        
+        audio.onplay = (): void => setIsPlaying (true)
+            
+        audio.onpause = (): void => setIsPlaying (false)
+
+        return (): void => {
+
+            setIsPlaying (false) 
+
+        }
+
+    }, [source, setIsPlaying, playerRef])
+
+    return <></>
+
+}
--- src/components/player.tsx
+++ src/components/player.tsx
@@ -1,13 +1,32 @@
 import React, { useEffect, useRef, ReactElement } from 'react'
-import { useRecoilState, useRecoilValue } from 'recoil'
-import { createBrowserHistory } from 'history'
+import { useRecoilState } from 'recoil'
 import 'react-toastify/dist/ReactToastify.css'
 import { PlayerControls } from './player.controls'
 import './player.styles.css'
-import { playerStateIsPlaying, playerStateSource, playerStateSpeed, playerStateRef } from './player.state'
+import { playerStateRef } from './player.state'
 import { PlayerIcons } from './player.icons'
 import { PlayerTitle } from './player.title'
 import { PlayerEvents } from './player.events'
+
+export const Player = (): ReactElement => {
+
+    return (
+        <>
+
+            <PlayerEvents />
+            
+            <PlayerTitle />
+
+            <PlayerIcons />
+
+            <PlayerElement />
+
+            <PlayerControls />
+
+        </>
+    )
+
+}
 
 const PlayerElement = (): ReactElement => {
 
@@ -29,65 +48,6 @@ const PlayerElement = (): ReactElement => {
                     <track kind="captions" />
                 </audio>
             </div>
-        </>
-    )
-
-}
-
-export const Player = (): ReactElement => {
-
-    const [, setIsPlaying] = useRecoilState (playerStateIsPlaying)
-    const playerRef = useRecoilValue (playerStateRef)
-    const source = useRecoilValue (playerStateSource)
-    const speed = useRecoilValue (playerStateSpeed)
-
-    useEffect (() => {
-        
-        const audio: any = playerRef
-
-        audio.mozPreservesPitch = false
-        
-        audio.src = source
-        
-        audio.load ()
-        
-        audio.onplay = (): void => setIsPlaying (true)
-            
-        audio.onpause = (): void => setIsPlaying (false)
-
-        return (): void => {
-
-            setIsPlaying (false)
-        
-        }
-        
-    }, [source, setIsPlaying, playerRef])
-
-    useEffect ((): void => {
-        
-        const audio: any = playerRef
-
-        audio.playbackRate = speed
-        
-        const history = createBrowserHistory ()
-            
-        history.push (`${window.location.pathname}?speed=${audio.playbackRate}`)
-        
-    }, [speed, playerRef])
-
-    return (
-        <>
-
-            <PlayerEvents />
-            
-            <PlayerTitle />
-
-            <PlayerIcons />
-
-            <PlayerElement />
-
-            <PlayerControls />
-
         </>
     )
 
