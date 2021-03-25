3f9eaaad3d8165fdb49a5191a4532fa9c7bdb3d1
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-03-25 12:29:33 +0100

    feat(indicators): speed to percentage from npm package

--- src/components/player-indicators-percent.js
+++ src/components/player-indicators-percent.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import speedToPercentage from 'speed-to-percentage'
 import { PlayerIndicatorsBlueprint } from './player-indicators-blueprint'
 
 export const PlayerIndicatorsPercent = (props) => {
 
     const { speed } = props
-    const speedToPercent = () => (speed * 100 - 100).toFixed (1)
 
-    return <PlayerIndicatorsBlueprint title='percent' value={speedToPercent ()}/>
+    return <PlayerIndicatorsBlueprint title='percent' value={speedToPercentage (speed, 1)}/>
 
 }
