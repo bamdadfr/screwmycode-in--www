76b17de7078240171c5e179cd4d34a0e930185b9
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-06-17 16:09:05 +0200

    fix(layout-toast): black background

--- src/components/layout-toast/layout-toast.component.js
+++ src/components/layout-toast/layout-toast.component.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import styled from 'styled-components'
+
+const StyledContainer = styled.div`
+    & > div > div > div {
+        background-color: black;
+    }
+`
 
 export default function LayoutToastComponent () {
 
     return (
         <>
-            <ToastContainer/>
+            <StyledContainer>
+                <ToastContainer/>
+            </StyledContainer>
         </>
     )
 
