2f8ee290dbab877cca11333ea487554e55e2aa35
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-06-17 05:30:21 +0200

    cleaned wip

--- src/components/layout/layout.component.js
+++ src/components/layout/layout.component.js
@@ -1,10 +1,8 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import LayoutBackgroundComponent from '@/components/layout-background/layout-background.component'
 import LayoutHeadComponent from '@/components/layout-head/layout-head.component'
 import LayoutMainComponent from '@/components/layout-main/layout-main.component'
-import usePlayer from '@/hooks/use-player'
-import getYoutubeInfo from '@/utils/get-youtube-info'
 
 const propTypes = {
     'children': PropTypes.node.isRequired,
@@ -18,15 +16,6 @@ const propTypes = {
  * @return {JSX.Element}
  */
 export default function LayoutComponent ({ children }) {
-
-    // usePlayer ()
-    useEffect (async () => {
-
-        const youtube = await getYoutubeInfo ()
-
-        console.log (youtube)
-    
-    }, [])
 
     return (
         <>
