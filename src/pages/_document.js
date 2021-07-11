f4bcd04272ccfcffeed343a3753f1a434799dc11
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-07-11 17:43:48 +0200

    feat(styles): add ssr to styled-components

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,44 @@
 /* eslint-disable react/no-danger */
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 import { GA_TRACKING_ID } from '../lib/gtag'
 
 export default class MyDocument extends Document {
+
+    static async getInitialProps (ctx) {
+
+        const sheet = new ServerStyleSheet ()
+        const originalRenderPage = ctx.renderPage
+
+        try {
+
+            ctx.renderPage = () =>
+                originalRenderPage ({
+                    'enhanceApp': (App) => (props) =>
+                        // eslint-disable-next-line react/jsx-props-no-spreading
+                        sheet.collectStyles (<App {...props} />),
+                })
+
+            const initialProps = await Document.getInitialProps (ctx)
+
+            return {
+                ...initialProps,
+                'styles': (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement ()}
+                    </>
+                ),
+            }
+
+        } finally {
+
+            sheet.seal ()
+
+        }
+
+    }
 
     render () {
 
