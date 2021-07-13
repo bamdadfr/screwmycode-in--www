e2db355d7765e5fd21f3716b90d39366bc603885
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-07-13 14:57:22 +0200

    feat(soundcloud): add page for soundcloud links

---
+++ src/pages/soundcloud/[user]/[id]/[speed].js
@@ -0,0 +1,164 @@
+import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+import SoundcloudScraper from 'soundcloud-scraper'
+import { useRouter } from 'next/router'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import Head from 'next/head'
+import PlayerComponent from '../../../../components/player/player.component'
+import { GetYoutubeThumbnailUtils } from '../../../../utils/get-youtube-thumbnail.utils'
+import { repeatAtom } from '../../../../atoms/repeat.atom'
+import { volumeAtom } from '../../../../atoms/volume.atom'
+import { StyledContainer, StyledTitle } from '../../../../pages-styles/youtube/[id]/[speed].styles'
+import IndicatorsComponent from '../../../../components/indicators/indicators.component'
+import SliderComponent from '../../../../components/slider/slider.component'
+
+const propTypes = {
+    'title': PropTypes.string.isRequired,
+    'url': PropTypes.string.isRequired,
+    'speed': PropTypes.number.isRequired,
+}
+
+/**
+ * @function
+ * @name SoundcloudPage
+ * @description /soundcloud/[user]/[id]/[speed]
+ * @param {object} props - props
+ * @param {string} props.title - audio title
+ * @param {string} props.url - audio url
+ * @param {number} props.speed - audio speed
+ * @returns {React.ReactElement} - react component
+ */
+export default function SoundcloudPage ({ title, url, 'speed': speedFromProps }) {
+
+    const router = useRouter ()
+    const [speed, setSpeed] = useState (speedFromProps)
+    const [description, setDescription] = useState (`${title} - ${speedFromProps} - ScrewMyCode.In`)
+    const [thumbnail] = useState (GetYoutubeThumbnailUtils (router.query.id))
+    const [autoplay, setAutoplay] = useState (false)
+    const repeat = useRecoilValue (repeatAtom)
+    const [volume, setVolume] = useRecoilState (volumeAtom)
+
+    /**
+     * @function
+     * @name onMount
+     * @description setup autoplay capability
+     */
+    async function onMount () {
+
+        // https://github.com/video-dev/can-autoplay/issues/36
+        import ('can-autoplay')
+            .then ((module) => module.default.video ())
+            .then (({ result }) => {
+
+                if (result === true) {
+
+                    setAutoplay (true)
+
+                } else {
+
+                    setAutoplay (false)
+
+                }
+
+            })
+
+    }
+
+    useEffect (onMount, [])
+
+    /**
+     * @function
+     * @name onSpeed
+     * @description update description + route
+     */
+    async function onSpeed () {
+
+        setDescription (`${title} - ${speed} - SoundCloud - ScrewMyCode.In`)
+
+        await router.push (
+            `/soundcloud/${router.query.user}/${router.query.id}/${speed}`,
+            undefined,
+            { 'shallow': true },
+        )
+
+    }
+
+    useEffect (onSpeed, [speed])
+
+    return (
+        <>
+            <Head>
+
+                <title>{description}</title>
+
+                <meta itemProp="description" content={description}/>
+                <meta itemProp="image" content={thumbnail}/>
+
+                <meta name="twitter:description" content={description}/>
+                <meta name="twitter:image" content={thumbnail}/>
+
+                <meta property="og:description" content={description}/>
+                <meta property="og:image" content={thumbnail}/>
+
+            </Head>
+            <StyledContainer>
+
+                <StyledTitle>
+                    {title}
+                </StyledTitle>
+
+                <PlayerComponent
+                    url={url}
+                    playbackRate={speed}
+                    loop={repeat}
+                    volume={volume}
+                    handleVolume={(v) => setVolume (parseFloat (v))}
+                    autoplay={autoplay}
+                />
+
+                <IndicatorsComponent
+                    value={speed}
+                />
+
+                <SliderComponent
+                    value={speed}
+                    handleValue={(s) => setSpeed (parseFloat (s))}
+                />
+
+            </StyledContainer>
+        </>
+    )
+
+}
+
+SoundcloudPage.propTypes = propTypes
+
+/**
+ * @function
+ * @name getServerSideProps
+ * @description sanitize url parameters + check if id is valid + get data from API
+ * @param {object} context - next.js context
+ * @returns {object} - props to pass
+ */
+export async function getServerSideProps (context) {
+
+    const { user, id, speed } = context.query
+    const props = {}
+    const scraper = new SoundcloudScraper.Client ()
+    const soundcloudUrl = `https://soundcloud.com/${user}/${id}`
+    const isValid = await SoundcloudScraper.Util.validateURL (soundcloudUrl)
+
+    if (!isValid) return { 'redirect': { 'destination': '/', 'permanent': false }}
+
+    const info = await scraper.getSongInfo (soundcloudUrl)
+    const audioUrl = await SoundcloudScraper.Util.fetchSongStreamURL (info.trackURL)
+
+    props.title = info.title
+
+    props.url = audioUrl
+
+    props.speed = parseFloat (speed) || 1
+
+    return { props }
+
+}
