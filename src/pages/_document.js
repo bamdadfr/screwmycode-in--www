97f5af8528e82f8be152d62c767b5560f96d39c6
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-07-16 11:42:28 +0200

    fix(HTML): add `lang`

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -44,7 +44,7 @@ export default class MyDocument extends Document {
 
         // noinspection HtmlRequiredTitleElement
         return (
-            <Html>
+            <Html lang="en">
                 <Head>
                     {/* Global Site Tag (gtag.js) - Google Analytics */}
                     <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}/>
