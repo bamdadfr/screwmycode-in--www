c388c810dc8e6989781da98246ac4286471f3138
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-07-17 16:54:23 +0200

    perf(tests): write new tests

---
+++ jest/index.js
@@ -0,0 +1,9 @@
+export { JestChildren } from './jest-children'
+
+export { JestInstance } from './jest-instance'
+
+export { JestRender } from './jest-render'
+
+export { JestUnit } from './jest-unit'
+
+export { JestWrapper } from './jest-wrapper'
