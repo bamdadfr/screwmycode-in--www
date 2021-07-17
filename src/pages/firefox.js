d998a8a6fc9441cec898cba6018ba0669b19f428
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-07-17 15:43:02 +0200

    feat(FirefoxPage): add champagne icon

--- src/pages/firefox.js
+++ src/pages/firefox.js
@@ -1,19 +1,35 @@
 import React from 'react'
+import Image from 'next/image'
 import styled from 'styled-components'
 import { DefaultLayout } from '../layouts'
 
 const Container = styled.div`
     display: flex;
-    justify-content: center;
+    justify-content: space-evenly;
     align-items: center;
-    flex-direction: column;
-    font-size: 1.1em;
+    width: 80%;
+    max-width: 20em;
+`
+
+const ImageContainer = styled.div`
+    position: relative;
+    height: 10em;
+    width: 4em;
+
+    > div {
+        transform: translate3d(0, -0.9em, 0);
+    }
+`
+
+const TextContainer = styled.div`
+    margin-left: 2em;
+    text-align: left;
 `
 
 const TextLink = styled.a`
-    font-weight: 900;
-    text-decoration: none;
-    border-bottom: 1px solid white;
+    font-weight: 600;
+    color: ${(props) => props.theme.highlight};
+    border-bottom: 0.15em solid ${(props) => props.theme.highlight};
 `
 
 /**
@@ -25,18 +41,27 @@ export default function FirefoxPage () {
         <>
             <DefaultLayout metaDescription="only available for firefox">
                 <Container>
-                    <h2>
-                        only available for firefox
-                    </h2>
-                    <div>
-                        <TextLink
-                            rel="noopener noreferrer"
-                            target="_blank"
-                            href="https://www.mozilla.org/firefox/new/"
-                        >
-                            download here
-                        </TextLink>
-                    </div>
+                    <ImageContainer>
+                        <Image
+                            src="/icons/SCRW_CHAMP.svg"
+                            layout="fill"
+                            objectFit="contain"
+                        />
+                    </ImageContainer>
+                    <TextContainer>
+                        <h2>
+                            only available for firefox
+                        </h2>
+                        <div>
+                            <TextLink
+                                rel="noopener noreferrer"
+                                target="_blank"
+                                href="https://www.mozilla.org/firefox/new/"
+                            >
+                                download here
+                            </TextLink>
+                        </div>
+                    </TextContainer>
                 </Container>
             </DefaultLayout>
         </>
