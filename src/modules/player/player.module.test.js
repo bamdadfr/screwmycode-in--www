d5cb4b5b71fa20ff05fa7e4efb9028f7be227991
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-08-03 19:16:33 +0200

    perf(PlayerModule): add basic unit test

---
+++ src/modules/player/player.module.test.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { render as defaultRender } from '@testing-library/react'
+import { PlayerModule } from './player.module'
+import { WithStyledComponents } from '../../app/components'
+
+const render = () => {
+
+    const { container } = defaultRender (
+        <WithStyledComponents>
+            <PlayerModule/>
+        </WithStyledComponents>,
+    )
+
+    return {
+        container,
+    }
+
+}
+
+describe ('PlayerModule', () => {
+
+    describe ('container', () => {
+
+        it ('should be in the document, visible and not empty', () => {
+
+            const { container } = render ()
+
+            expect (container).toBeInTheDocument ()
+
+            expect (container).toBeVisible ()
+
+            expect (container).not.toBeEmptyDOMElement ()
+        
+        })
+    
+    })
+
+})
