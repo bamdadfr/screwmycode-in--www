b7bd2b66b83481e4137710818c08b5f0f5f3f902
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-08-03 03:36:54 +0200

    fix(TitleComponent): fix hover style

--- src/components/title/title.component.styles.js
+++ src/components/title/title.component.styles.js
@@ -5,19 +5,22 @@ export const Title = styled.h1`
     display: flex;
     justify-content: center;
     align-items: flex-end;
-    
+
     height: 100%;
 
     margin-bottom: 3em;
-    
+
     font-size: 3em;
     font-weight: 900;
-    transition: color 200ms ease-in-out;
 
-    &:hover {
-        color: ${(props) => props.theme.highlight};
+    & a {
+        transition: color 200ms ease-in-out;
+
+        &:hover {
+            color: ${(props) => props.theme.highlight};
+        }
     }
-    
+
     @media screen and (max-width: ${Breakpoints.tablet - 1}px) {
         font-size: 2em;
     }
