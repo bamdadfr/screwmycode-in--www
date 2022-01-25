e967bdd01ca4723784c14f3c81df5d41ede45253
Bamdad Sabbagh <hi@bamdad.fr>
2022-01-25 13:59:43 +0100

    fix: Adjust color and blur of artwork image

--- src/modules/player/components/image/image.component.styles.js
+++ src/modules/player/components/image/image.component.styles.js
@@ -9,9 +9,9 @@ export const Image = styled.img`
   z-index: -1;
   filter: ${({speed}) => {
     const sepia = Math.round(mapRange(speed, 0.84, 1, 100, 0, true));
-    const hueRotate = Math.round(mapRange(speed, 0.84, 1, 280, 0, true));
+    const hueRotate = Math.round(mapRange(speed, 0.84, 1, 250, 0, true));
     const saturate = Math.round(mapRange(speed, 0.84, 1, 500, 100, true));
-    const blur = Math.round(mapRange(speed, 0.84, 1, 10, 3, true));
+    const blur = Math.round(mapRange(speed, 0.84, 1, 10, 5, true));
 
     return `
       sepia(${sepia}%)
