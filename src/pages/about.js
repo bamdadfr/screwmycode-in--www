2e87c35788b51f7e27238261cc151b7797b03976
Bamdad Sabbagh <hi@bamdad.fr>
2022-01-26 14:40:35 +0100

    feat(about): add under construction page

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {DefaultLayout} from '../layouts/default/default.layout';
+
+/**
+ * About page
+ * Path: /about
+ *
+ * @returns {React.ReactElement} - About page component
+ */
+export default function AboutPage() {
+  return (
+    <>
+      <DefaultLayout>
+        Under construction
+      </DefaultLayout>
+    </>
+  );
+}
