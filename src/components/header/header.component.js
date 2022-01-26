437be915b15ed4f6c1027f9343cbb1dce0dacbc6
Bamdad Sabbagh <hi@bamdad.fr>
2022-01-26 14:40:01 +0100

    feat(header): add `latest`, `top` and `about` buttons

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,8 +1,17 @@
+// noinspection HtmlUnknownTarget
+
 import React from 'react';
 import Link from 'next/link';
 import home from '@iconify/icons-mdi/home';
+import history from '@iconify/icons-mdi/history';
+import podium from '@iconify/icons-mdi/podium';
+import information from '@iconify/icons-mdi/information-variant';
 import {Icon} from '@iconify/react';
-import {Container} from './header.component.styles';
+import {
+  LeftContainer,
+  PodiumButton,
+  RightContainer,
+} from './header.component.styles';
 
 /**
  * Component for the header of the application
@@ -12,7 +21,7 @@ import {Container} from './header.component.styles';
 export function HeaderComponent() {
   return (
     <>
-      <Container>
+      <LeftContainer>
         <Link href="/">
           <button
             type="button"
@@ -21,7 +30,33 @@ export function HeaderComponent() {
             <Icon icon={home} />
           </button>
         </Link>
-      </Container>
+        <Link href="/latest">
+          <button
+            type="button"
+            aria-label="latest"
+          >
+            <Icon icon={history} />
+          </button>
+        </Link>
+        <Link href="/top">
+          <PodiumButton
+            type="button"
+            aria-label="latest"
+          >
+            <Icon icon={podium} />
+          </PodiumButton>
+        </Link>
+      </LeftContainer>
+      <RightContainer>
+        <Link href="/about">
+          <button
+            type="button"
+            aria-label="latest"
+          >
+            <Icon icon={information} />
+          </button>
+        </Link>
+      </RightContainer>
     </>
   );
 }
