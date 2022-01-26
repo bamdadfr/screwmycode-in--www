1fd60b9b7b1b4c9e558308037a706b79e6ddd962
Bamdad Sabbagh <hi@bamdad.fr>
2022-01-26 14:40:40 +0100

    feat(top): add under construction page

---
+++ src/pages/top.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {DefaultLayout} from '../layouts/default/default.layout';
+
+/**
+ * Top page
+ * Path: /top
+ *
+ * @returns {React.ReactElement} - Top page component
+ */
+export default function TopPage() {
+  return (
+    <>
+      <DefaultLayout>
+        Under construction
+      </DefaultLayout>
+    </>
+  );
+}
