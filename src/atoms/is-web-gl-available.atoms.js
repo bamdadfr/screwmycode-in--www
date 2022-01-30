24af81a352e1a3b1712f5e0737a14914a6b1e418
Bamdad Sabbagh <hi@bamdad.fr>
2022-01-30 15:36:45 +0100

    fix(WebGL): Add WebGL2 verification

--- src/atoms/is-web-gl-available.atoms.js
+++ src/atoms/is-web-gl-available.atoms.js
@@ -7,12 +7,26 @@ import {atom} from 'jotai';
  * @returns {boolean} - True if WebGL is supported, false otherwise.
  */
 function isWebGlAvailable() {
+  let isWebGl;
+  let isWebGl2;
+
+  // WebGL
   try {
     const canvas = document.createElement('canvas');
-    return !!(window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl')));
+    isWebGl = !!(window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl')));
   } catch (e) {
-    return false;
+    isWebGl = false;
   }
+
+  // WebGL2
+  try {
+    const canvas = document.createElement('canvas');
+    isWebGl2 = !!(window.WebGL2RenderingContext && canvas.getContext('webgl2'));
+  } catch (e) {
+    isWebGl2 = false;
+  }
+
+  return isWebGl === true && isWebGl2 === true;
 }
 
 export const isWebGlAvailableAtom = atom(isWebGlAvailable());
