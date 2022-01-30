433a260ef9849a764cb88ed6e3bd8afe453c6965
Bamdad Sabbagh <hi@bamdad.fr>
2022-01-30 15:30:19 +0100

    perf(Breakpoints): Remove old breakpoints in favor of defaults from `styled-breakpoints` package

--- src/app/styles/breakpoints.js
+++ src/app/styles/breakpoints.js
@@ -1,14 +1,5 @@
 import {createTheme} from 'styled-breakpoints';
 
-export const BreakpointsOld = createTheme({
-  xs: '0px',
-  sm: '576px',
-  md: '768px',
-  lg: '1024px',
-  xl: '1216px',
-  xxl: '1408px',
-});
-
 export const Breakpoints = createTheme({
   xs: '0px',
   sm: '576px',
