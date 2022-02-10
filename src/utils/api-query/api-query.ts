19df649a2c2f27936322f41571a570fee983a5ae
Bamdad Sabbagh <hi@bamdad.fr>
2022-02-10 12:26:57 +0100

    fix(apiQuery): Remove redundant `await` in return

--- src/utils/api-query/api-query.ts
+++ src/utils/api-query/api-query.ts
@@ -2,5 +2,5 @@ import ky from 'ky';
 import {API_BASE_URL} from '../../constants';
 
 export async function apiQuery<R>(endpoint: string): Promise<R> {
-  return await ky.get(`${API_BASE_URL}${endpoint}`).json();
+  return ky.get(`${API_BASE_URL}${endpoint}`).json();
 }
