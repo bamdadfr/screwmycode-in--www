af31eb97117b3c8a525969250395bcb533df6958
Bamdad Sabbagh <hi@bamdad.fr>
2022-05-07 12:33:07 +0200

    feat: Add sitemap + Update dependencies + Add env config example

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+/* eslint-disable jsdoc/valid-types */
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: 'https://www.screwmycode.in/',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
