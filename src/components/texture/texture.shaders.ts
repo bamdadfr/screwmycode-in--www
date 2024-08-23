5d123b7f7c5a0a4b7200c85ef81dc0694db4aac3
Bamdad Sabbagh <hi@bamdad.fr>
2024-08-23 16:08:17 +0200

    fix: remove dead code

--- src/components/texture/texture.shaders.ts
+++
@@ -1,8 +0,0 @@
-import {GLSL, Shaders} from 'gl-react';
-import fragmentShader from 'src/components/texture/texture.shaders.fragment.glsl';
-
-export const shaders = Shaders.create({
-  screw: {
-    frag: GLSL`${fragmentShader}`,
-  },
-});
