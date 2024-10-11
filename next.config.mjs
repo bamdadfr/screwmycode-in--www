7d19ed11b8e82cd082640cdb4b4115d751de2ac1
Bamdad Sabbagh <hi@bamdad.fr>
2024-10-11 02:09:38 +0200

    fix: remove new remotes (unuseful)

--- next.config.mjs
+++ next.config.mjs
@@ -14,18 +14,6 @@ const config = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'i.ytimg.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.bcbits.com',
-        port: '',
-        pathname: '/**',
-      },
     ],
   },
   webpack: (c) => {
