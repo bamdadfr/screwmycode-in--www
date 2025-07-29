d0543b5bc9898b53b16cbf594e8130676c51f6b2
Bamdad Sabbagh <hi@bamdad.fr>
2025-07-29 11:05:25 +0200

    fix: cleanup webgl resources properly

--- src/components/artwork/artwork-texture.tsx
+++ src/components/artwork/artwork-texture.tsx
@@ -18,8 +18,9 @@ const r0 = 0;
 export const ArtworkTexture = ({width, height, texture, f0 = false}: Props) => {
   const {isReset, setIsReset} = useArtworkReset();
 
-  const t = useRef(t0);
-  const r = useRef(r0);
+  const timeRef = useRef(t0);
+  const ratioRef = useRef(r0);
+  const materialRef = useRef<ShaderMaterial | null>(null);
 
   // const s = useMemo(
   //   () =>
@@ -29,24 +30,28 @@ export const ArtworkTexture = ({width, height, texture, f0 = false}: Props) => {
   //   [speed, isPlaying],
   // );
 
-  const material = useMemo(
-    () =>
-      new ShaderMaterial({
-        uniforms: {
-          u_texture: {value: texture},
-          u_ratio: {value: r.current},
-          u_time: {value: t.current},
-        },
-        vertexShader,
-        fragmentShader,
-      }),
-    [texture],
-  );
+  const material = useMemo(() => {
+    if (materialRef.current) {
+      materialRef.current.dispose();
+    }
+
+    materialRef.current = new ShaderMaterial({
+      uniforms: {
+        u_texture: {value: texture},
+        u_ratio: {value: ratioRef.current},
+        u_time: {value: timeRef.current},
+      },
+      vertexShader,
+      fragmentShader,
+    });
+
+    return materialRef.current;
+  }, [texture]);
 
   const reset = useCallback(() => {
-    t.current = t0;
-    r.current = r0;
-  }, [t, r]);
+    timeRef.current = t0;
+    ratioRef.current = r0;
+  }, [timeRef, ratioRef]);
 
   useEffect(() => {
     if (!isReset) {
@@ -63,12 +68,21 @@ export const ArtworkTexture = ({width, height, texture, f0 = false}: Props) => {
     }
 
     const x = Math.random() * 0.004;
-    t.current += x;
-    r.current += x;
+    timeRef.current += x;
+    ratioRef.current += x;
 
-    material.uniforms.u_time.value = t.current;
-    material.uniforms.u_ratio.value = r.current;
+    material.uniforms.u_time.value = timeRef.current;
+    material.uniforms.u_ratio.value = ratioRef.current;
   });
+
+  // cleanup
+  useEffect(() => {
+    return () => {
+      if (materialRef.current) {
+        materialRef.current.dispose();
+      }
+    };
+  }, [materialRef]);
 
   return (
     <mesh>
--- src/components/artwork/artwork.tsx
+++ src/components/artwork/artwork.tsx
@@ -1,8 +1,8 @@
 import {Canvas} from '@react-three/fiber';
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import {ArtworkTexture} from 'src/components/artwork/artwork-texture';
 import {TEXTURE_SCALE} from 'src/constants';
-import {type Texture, TextureLoader} from 'three';
+import {type Texture, TextureLoader, WebGLRenderer} from 'three';
 
 interface Props {
   url: string;
@@ -19,17 +19,32 @@ export const Artwork = ({
   freeze0 = false,
   scale = TEXTURE_SCALE,
 }: Props) => {
-  const [texture, setTexture] = useState<Texture | null>(null);
-  const [isLoaded, setLoaded] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const rendererRef = useRef<WebGLRenderer | null>(null);
+  const textureRef = useRef<Texture | null>(null);
 
+  // loader
   useEffect(() => {
-    const onLoad = () => {
-      setLoaded(true);
+    const onLoad = (texture: Texture) => {
+      textureRef.current = texture;
+      setIsLoaded(true);
     };
 
-    const t = new TextureLoader().load(url, onLoad);
-    setTexture(t);
-  }, [url]);
+    new TextureLoader().load(url, onLoad);
+  }, [url, textureRef]);
+
+  // cleanup
+  useEffect(() => {
+    return () => {
+      if (textureRef.current) {
+        textureRef.current.dispose();
+      }
+
+      if (rendererRef.current) {
+        rendererRef.current.dispose();
+      }
+    };
+  }, [rendererRef]);
 
   return (
     <Canvas
@@ -37,13 +52,19 @@ export const Artwork = ({
         opacity: isLoaded ? 1 : 0,
         top: isLoaded ? 0 : '1px',
       }}
+      gl={{
+        powerPreference: 'low-power',
+      }}
+      onCreated={({gl}) => {
+        rendererRef.current = gl;
+      }}
     >
-      {texture && (
+      {textureRef.current && (
         <ArtworkTexture
-          key={JSON.stringify(url)}
+          key={url}
           width={width * scale}
           height={height * scale}
-          texture={texture}
+          texture={textureRef.current}
           f0={freeze0}
         />
       )}
