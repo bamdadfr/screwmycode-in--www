cb4fb58558f3c1dda1441a41a3886100e2d8a026
Bamdad Sabbagh <hi@bamdad.fr>
2025-07-31 22:15:56 +0200

    feat: add lazy loading for cards

--- src/components/card-container/card-container.tsx
+++ src/components/card-container/card-container.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import {useEffect, useState} from 'react';
 import {useAudioState} from 'src/components/app/hooks/use-audio-state';
 import {Card} from 'src/components/card/card';
 import styles from 'src/components/card-container/card-container.module.scss';
@@ -10,20 +11,40 @@ interface Props {
   token: string;
 }
 
+const ID = '#load-more-sentinel';
+const L = 10;
+
 export function CardContainer({medias}: Props) {
   const {isLoading} = useAudioState();
+  const [visibleCards, setVisibleCards] = useState(L);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        setVisibleCards((prev) => prev + L);
+      }
+    });
+
+    const sentinel = document.querySelector(ID);
+
+    if (sentinel) {
+      observer.observe(sentinel);
+    }
+
+    return () => observer.disconnect();
+  }, []);
 
   return (
     <>
       {isLoading && <div className={styles.overlay} />}
-      {medias.map((item) => {
-        return (
-          <Card
-            key={item.url}
-            media={item}
-          />
-        );
-      })}
+      {medias.slice(0, visibleCards).map((item) => (
+        <Card
+          key={item.url}
+          media={item}
+        />
+      ))}
+
+      {visibleCards < medias.length && <div id="load-more-sentinel" />}
     </>
   );
 }
